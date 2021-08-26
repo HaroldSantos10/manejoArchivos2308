ee6a1b231902a72393b3dbfdfcd5b4213af45f5e
Harold Santos <haroldsan9.9@gmail.com>
2021-08-26 09:33:23 -0600

    Repositorio Semana 5

---
+++ manejoArchivos2308/FileExample.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace manejoArchivos2308
+{
+    class FileExample
+    {
+        static void Main()
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = Menu();// llamado al menú
+            }
+            Console.ReadKey();
+        }
+
+        private static bool Menu()
+        {
+            //Crear el menu para mostrar5 al usuario 
+            Console.Clear(); // permite limpiar la consola
+            Console.WriteLine("Seleccione la operación a realizar: ");
+            Console.WriteLine("1. Registrar nuevo estudiante");
+            Console.WriteLine("2. Actualizar datos de estudiante");
+            Console.WriteLine("3. Eliminar datos de estudiante");
+            Console.WriteLine("4. Mostrar listado de estudiantes");
+            Console.WriteLine("5. Salir");
+            Console.Write("\nOpcion: "); //salto de línea
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    register(); //llamado al metodo registrar
+                    return true;
+                case "2":
+                    updateData(); //llamdo al metodo para actualizar
+                    Console.ReadKey();
+                    return true;
+                case "3":
+                    return true;
+                case "4":
+                    //mostrar el contenido del archivo
+                    Console.WriteLine("LISTADO DE ESTUDIANTES");
+                    foreach (KeyValuePair<object, object> data in readFile())
+                    {
+                        Console.WriteLine("{0}: {1}", data.Key, data.Value);
+                    }
+                    Console.ReadKey();
+                    return true;
+                case "5":
+                    return false;
+                default:
+                    return false;
+
+            }
+
+        }
+
+        //metodo para obtener la ruta del archivo
+        private static string getPath()
+        {
+            string path = @"C:\ejemplosC#\resgistros.txt";
+            return path;
+        }
+
+        //metodo para registrar datos en el archivo
+        private static void register()
+        {
+            //solicitar los datos del estudiante
+            Console.WriteLine("DATOS DEL ESTUDIANTE");
+            Console.Write("Nombre Completo: ");
+            string fullname = Console.ReadLine();
+            Console.Write("Edad: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //crear el archivo, uso de StreamWriter para escribir el archivo
+            using (StreamWriter sw = File.AppendText(getPath()))
+            {
+                sw.WriteLine("{0}; {1}", fullname, age);
+                sw.Close();
+            }
+        }
+
+        //metodo para leer el contenido del archivo
+        private static Dictionary<object, object> readFile()
+        {
+            //declarar el diccionario
+            Dictionary<object, object> listData = new Dictionary<object, object>();
+
+            //uso del StreamReader para leer el archivo
+            using (var reader = new StreamReader(getPath()))
+            {
+                //variable para almacenar el contenido del archivo
+                string lines;
+
+                while ((lines = reader.ReadLine()) != null) //mientras no se encuentre una linea vacia se ejecuta el ciclo
+                {
+                    string[] keyvalue = lines.Split(';');
+                    if (keyvalue.Length == 2)
+                    {
+                        listData.Add(keyvalue[0], keyvalue[1]);
+                    }
+                }
+
+            }
+
+            //retornar el diccionario
+            return listData;
+        }
+
+        //metodo para buscar por clave
+        private static bool search(string name)
+        {
+            if (!readFile().ContainsKey(name))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //metodo para actualizar
+        private static void updateData()
+        {
+            //solicitar el elemnto a modificar
+            Console.Write("Escriba el nombre del estudiante a actualizar: ");
+            var name = Console.ReadLine();
+
+            //realizar la busqueda
+            if (search(name))
+            {
+                Console.WriteLine("El registro existe!");
+                Console.Write("Nueva edad: ");
+                var newAge = Console.ReadLine();
+
+                //declarar un diccionario
+                Dictionary<object, object> temp = new Dictionary<object, object>();
+                temp = readFile();
+
+                temp[name] = newAge; //actualizar el valor
+                Console.WriteLine("El registro ha sido actualizado!");
+                File.Delete(getPath()); //eliminamos archivos y posteriormente lo volvemos a crear
+
+                using (StreamWriter sw = File.AppendText(getPath()))
+                {
+                    //leer diccionario temporal y almacenar los elementos en el archivo
+                    foreach (KeyValuePair<object, object> values in temp)
+                    {
+                        sw.WriteLine("{0}; {1}", values.Key, values.Value);
+                        // sw.Close();
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("El registro no se encontro!");
+            }
+
+
+        }
+
+        private static void deleteData()
+        {
+            //solicitar el elemnto a eliminar
+            Console.Write("Escriba el nombre del estudiante a eliminar: ");
+            var name = Console.ReadLine();
+
+            //realizar la busqueda
+            if (search(name))
+            {
+
+
+                //declarar un diccionario
+                Dictionary<object, object> temp = new Dictionary<object, object>();
+                temp = readFile();
+
+                temp.Remove(name);
+                Console.WriteLine("El registro ha sido eliminado!");
+                File.Delete(getPath()); //eliminamos archivos y posteriormente lo volvemos a crear
+
+                using (StreamWriter sw = File.AppendText(getPath()))
+                {
+                    //leer diccionario temporal y almacenar los elementos en el archivo
+                    foreach (KeyValuePair<object, object> values in temp)
+                    {
+                        sw.WriteLine("{0}; {1}", values.Key, values.Value);
+                        // sw.Close();
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("El registro no se encontro!");
+            }
+
+        }
+
+}   }
---
+++ manejoArchivos2308/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //permitira trabajar con las clases para manipular archivos 
+
+namespace manejoArchivos2308
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //definir la ruta del archivo 
+            string path = @"C:\ejemplosC#\datos.txt";
+
+            //evaluar si el archivo existe para ya no crearlo
+            if (File.Exists(path))
+            {
+                Console.WriteLine("El archivo ya esxiste!");
+
+                //Leer el contenido del archivo linea por linea
+                String[] lines;
+                lines = File.ReadAllLines(path);
+                for (int i = 0; i< lines.Length; i++)
+                {
+                    Console.WriteLine(lines[i]);
+                }
+
+                //leer el contenido del archivo
+                Console.WriteLine("CONTENIDO UTILIZANDO ReadAllLines()");
+                String content;
+                content = File.ReadAllText(path);
+                Console.WriteLine(content);
+
+
+            }
+            else
+            {
+                 //crear un archivo, y agregar texto
+                using (StreamWriter sr = File.AppendText(path))
+                {
+                    //contenido del archivo
+                    sr.WriteLine("Programación computacional I");
+                    sr.WriteLine("Clase teorica | Stream I/O");
+                    sr.Close();
+                }
+
+            }
+
+
+
+            
+        }
+    }
+}
